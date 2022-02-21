fbac9ab6519f74df5a7639cb6b5948b83803ac6b
omarsalem7 <omarsalem0721@gmail.com>
2022-02-21 19:28:00 +0200

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import TodoContainer from './components/TodoContainer';
 import './App.css';
